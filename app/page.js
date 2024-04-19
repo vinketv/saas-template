97bb07d8ee83822d3c6350450d3a42ebbc295cc7
kevin sanches <kevin.sanches.ytb@gmail.com>
2024-04-19 11:50:09 +0200

    add custom picture for features section

--- app/page.js
+++ app/page.js
@@ -92,6 +92,7 @@ export default function Home() {
             </div>
             <img
               src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+              //src="/picture.png"
               alt="Product screenshot"
               className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
               width={2432}

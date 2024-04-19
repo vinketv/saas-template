da30826026b24617657594caed2b5bd75b0bd6ed
kevin sanches <kevin.sanches.ytb@gmail.com>
2024-04-19 11:43:27 +0200

    add react icon and select shadcn

--- app/page.js
+++ app/page.js
@@ -1,32 +1,35 @@
 import { Footer } from "@/components/Footer";
-import Image from "next/image";
+import { FaCloudUploadAlt, FaDatabase, FaLock } from "react-icons/fa";
 
 export default function Home() {
-
   const features = [
     {
-      name: 'Push to deploy.',
+      name: "Push to deploy.",
       description:
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-      icon: "",
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      icon: (
+        <FaCloudUploadAlt className="absolute left-1 top-1 h-5 w-5 text-black" />
+      ),
     },
     {
-      name: 'SSL certificates.',
-      description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
-      icon: "",
+      name: "SSL certificates.",
+      description:
+        "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
+      icon: <FaDatabase className="absolute left-1 top-1 h-5 w-5 text-black" />,
     },
     {
-      name: 'Database backups.',
-      description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-      icon: "",
+      name: "Database backups.",
+      description:
+        "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+      icon: <FaLock className="absolute left-1 top-1 h-5 w-5 text-black" />,
     },
-  ]
+  ];
 
   const stats = [
-    { id: 1, name: 'Trip generate this year', value: '2 million' },
-    { id: 2, name: 'Assets under holding', value: '$119 trillion' },
-    { id: 3, name: 'New users annually', value: '46,000' },
-  ]
+    { id: 1, name: "Generate profit this year", value: "2 million" },
+    { id: 2, name: "Assets under holding", value: "$119 trillion" },
+    { id: 3, name: "New users annually", value: "46,000" },
+  ];
 
   return (
     <>
@@ -34,14 +37,15 @@ export default function Home() {
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true"
-        >
-        </div>
+        ></div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-40">
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-black sm:text-6xl">
               Template for your SAAS business
             </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et tellus lacus. Morbi quis lacus id odio elementum interdum vel.
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              et tellus lacus. Morbi quis lacus id odio elementum interdum vel.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
@@ -61,19 +65,25 @@ export default function Home() {
           <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
             <div className="lg:pr-8 lg:pt-4">
               <div className="lg:max-w-lg">
-                <h2 className="text-base font-semibold leading-7 text-black">Deploy faster</h2>
-                <p className="mt-2 text-3xl font-bold tracking-tight text-black sm:text-4xl">A better workflow</p>
+                <h2 className="text-base font-semibold leading-7 text-black">
+                  Deploy faster
+                </h2>
+                <p className="mt-2 text-3xl font-bold tracking-tight text-black sm:text-4xl">
+                  A better workflow
+                </p>
                 <p className="mt-6 text-lg leading-8 text-gray-600">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
-                  iste dolor cupiditate blanditiis ratione.
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Maiores impedit perferendis suscipit eaque, iste dolor
+                  cupiditate blanditiis ratione.
                 </p>
                 <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
                   {features.map((feature) => (
                     <div key={feature.name} className="relative pl-9">
                       <dt className="inline font-semibold text-black">
-                        {/* <feature.icon className="absolute left-1 top-1 h-5 w-5 text-indigo-600" aria-hidden="true" /> */}
+                        {feature.icon}
+
                         {feature.name}
-                      </dt>{' '}
+                      </dt>{" "}
                       <dd className="inline">{feature.description}</dd>
                     </div>
                   ))}
@@ -96,8 +106,13 @@ export default function Home() {
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
             {stats.map((stat) => (
-              <div key={stat.id} className="mx-auto flex max-w-xs flex-col gap-y-4">
-                <dt className="text-base leading-7 text-gray-600">{stat.name}</dt>
+              <div
+                key={stat.id}
+                className="mx-auto flex max-w-xs flex-col gap-y-4"
+              >
+                <dt className="text-base leading-7 text-gray-600">
+                  {stat.name}
+                </dt>
                 <dd className="order-first text-3xl font-semibold tracking-tight text-black sm:text-5xl">
                   {stat.value}
                 </dd>
--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,52 +1,93 @@
-import Link from "next/link"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import Link from "next/link";
+import { FaDiscord, FaTwitter } from "react-icons/fa";
 
 export const Footer = () => {
-    return (
-        <>
-            <div className="absolute w-full py-10 rounded-md shadow-2xl">
-                <div className="mx-auto grid grid-rows-5 md:grid-rows-none md:grid-cols-5 gap-16 max-w-screen-2xl py-10 px-14 text-center md:text-left">
-                    <div>
-                        <p className="font-medium mb-3 text-gray-900">Language</p>
-                        <select className="rounded border-2 border-slates-900 px-8">
-                            <option>English</option>
-                        </select>
-                    </div>
-                    <div>
-                        <p className="font-medium mb-3 text-gray-900">Product</p>
-                        <ul className="flex flex-col items-center md:items-start gap-2">
-                            <li className="text-slate-500 hover:text-black"><Link href="/">Advertise</Link></li>
-                        </ul>
-                    </div>
-                    <div>
-                        <p className="font-medium mb-3 text-gray-900">Ressource</p>
-                        <ul className="flex flex-col items-center md:items-start gap-2">
-                        <li className="text-slate-500 hover:text-black"><Link href="/">List your business</Link></li>
-                            <li className="text-slate-500 hover:text-black"><Link href="/">Support & FAQ</Link></li>
-                        </ul>
-                    </div>
-                    <div>
-                        <p className="font-medium mb-3 text-gray-900">Infos</p>
-                        <ul className="flex flex-col items-center md:items-start gap-2">
-                            <li className="text-slate-500 hover:text-black"><Link href="/">About us</Link></li>
-                            <li className="text-slate-500 hover:text-black"><Link href="/">Feedback</Link></li>
-                            <li className="text-slate-500 hover:text-black"><Link href="/">Terms & Conditions</Link></li>
-                            <li className="text-slate-500 hover:text-black"><Link href="/">Contact</Link></li>
-                        </ul>
-                    </div>
-                    <div>
-                        <p className="font-medium mb-3 text-gray-900">Social</p>
-                        <ul className="flex flex-col items-center md:items-start gap-2">
-                            <li className="text-slate-500 hover:text-black"><Link href="/">Discord</Link></li>
-                            <li className="text-slate-500 hover:text-black"><Link href="/">Twitter</Link></li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="border-b-2 border-slate-500 w-1/2 m-auto" />
-                <div className="mt-10 text-center">
-                    <p className="text-slate-500">© Flyosophy 2024.</p>
-                    <p className="text-slate-500">A project by <Link href="https://github.com/vinketv" target="_blank" className="text-slate-900 font-medium cursor-pointer hover:border-b-2 hover:border-black">Kevin Sanches</Link></p>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="absolute w-full py-10 rounded-md shadow-2xl">
+        <div className="mx-auto grid grid-rows-5 md:grid-rows-none md:grid-cols-5 gap-16 max-w-screen-2xl py-10 px-14 justify-center text-center md:text-left">
+          <div className="flex flex-col">
+            <p className="font-medium mb-3 text-gray-900">Language</p>
+            <Select>
+              <SelectTrigger className="w-auto max-w-36 min-w-28">
+                <SelectValue placeholder="Language" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="light">English</SelectItem>
+                <SelectItem value="dark">Français</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <div>
+            <p className="font-medium mb-3 text-gray-900">Product</p>
+            <ul className="flex flex-col items-center md:items-start gap-2">
+              <li className="text-slate-500 hover:text-black">
+                <Link href="/">Advertise</Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <p className="font-medium mb-3 text-gray-900">Ressource</p>
+            <ul className="flex flex-col items-center md:items-start gap-2">
+              <li className="text-slate-500 hover:text-black">
+                <Link href="/">List your business</Link>
+              </li>
+              <li className="text-slate-500 hover:text-black">
+                <Link href="/">Support & FAQ</Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <p className="font-medium mb-3 text-gray-900">Infos</p>
+            <ul className="flex flex-col items-center md:items-start gap-2">
+              <li className="text-slate-500 hover:text-black">
+                <Link href="/">About us</Link>
+              </li>
+              <li className="text-slate-500 hover:text-black">
+                <Link href="/">Feedback</Link>
+              </li>
+              <li className="text-slate-500 hover:text-black">
+                <Link href="/">Terms & Conditions</Link>
+              </li>
+              <li className="text-slate-500 hover:text-black">
+                <Link href="/">Contact</Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <p className="font-medium mb-3 text-gray-900">Social</p>
+            <ul className="flex flex-col items-center md:items-start gap-2">
+              <li className="text-slate-500 hover:text-black cursor-pointer">
+                <FaDiscord className="w-6 h-6" />
+              </li>
+              <li className="text-slate-500 hover:text-black cursor-pointer">
+                <FaTwitter className="w-6 h-6" />
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="border border-slate-500 w-1/2 m-auto" />
+        <div className="mt-10 text-center">
+          <p className="text-slate-500">© Template 2024.</p>
+          <p className="text-slate-500">
+            A project by{" "}
+            <Link
+              href="https://github.com/vinketv"
+              target="_blank"
+              className="text-slate-900 font-medium cursor-pointer hover:border-b-2 hover:border-black"
+            >
+              Kevin Sanches
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };

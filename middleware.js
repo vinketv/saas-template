e8530bfea8db890585deb312c8c587530dfdbd32
kevin sanches <kevin.sanches.ytb@gmail.com>
2024-05-02 10:27:01 +0200

    fix signin redirect if user is connected

--- middleware.js
+++ middleware.js
@@ -2,13 +2,22 @@ import { NextResponse } from "next/server";
 import { auth } from "./auth";
 
 export default auth((req) => {
-  if (!req.auth) {
-    return NextResponse.redirect(new URL("/signin", req.url));
+  const isUserLoggedIn = req.auth;
+  const { pathname } = req.nextUrl;
+
+  // Rediriger l'utilisateur connecté
+  if (isUserLoggedIn && pathname === "/signin") {
+    return NextResponse.redirect(new URL("/dashboard", req.url)); // Redirige vers la page dashboard
+  }
+
+  // Rediriger l'utilisateur non connecté
+  if (!isUserLoggedIn) {
+    return NextResponse.redirect(new URL("/signin", req.url)); // Redirige vers la page de connexion
   }
 
   return NextResponse.next();
 });
 
 export const config = {
-  matcher: ["/dashboard/:function*"],
+  matcher: ["/signin", "/dashboard/:function*"],
 };

3517e1aa4d3b26531b9f6a763d767351d71b92b4
Vinke <kevinmorgane@hotmail.fr>
2024-05-26 20:16:15 +0200

    fix sidebar background for sous-domaine

--- components/SideBar/index.js
+++ components/SideBar/index.js
@@ -96,7 +96,11 @@ export function SideBar({ role }) {
                 <Link
                   href={feature.href}
                   className={`${
-                    pathname === feature.href ? "bg-gray-100" : ""
+                    (pathname.startsWith(feature.href) &&
+                      feature.href !== "/dashboard") ||
+                    pathname === feature.href
+                      ? "bg-gray-100"
+                      : ""
                   } flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group`}
                 >
                   {feature.icon}

7fc8a9cfb8f085fa5f4c1d1bd4dc2e08f1d5938f
Vinke <kevinmorgane@hotmail.fr>
2024-06-22 22:19:19 +0200

    add endpoint beta password check add check number of beta member

--- app/signin/layout.js
+++ app/signin/layout.js
@@ -1,16 +1,28 @@
+import { BetaClosed } from "@/components/Beta/BetaClosed";
 import { BetaForm } from "@/components/Beta/BetaForm";
+import { prisma } from "@/lib/prisma";
 import { cookies } from "next/headers";
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
   const cookieStore = cookies();
   const betaPasswordCookie = cookieStore.get("betaPassword");
   const betaPassword = betaPasswordCookie ? betaPasswordCookie.value : null;
+  const countBetaMember = await prisma.user.count({
+    where: {
+      plan: "BETA",
+    },
+  });
 
   if (
-    process.env.NEXT_PUBLIC_BETA_MODE === "true" &&
-    betaPassword !== process.env.BETA_PASSWORD
+    countBetaMember >= process.env.BETA_MEMBER_MAX &&
+    process.env.NEXT_PUBLIC_BETA_MODE === "true"
   ) {
-    console.log("test");
+    return <BetaClosed />;
+  } else if (
+    process.env.NEXT_PUBLIC_BETA_MODE === "true" &&
+    betaPassword !== process.env.BETA_PASSWORD &&
+    countBetaMember < process.env.BETA_PASSWORD
+  ) {
     return <BetaForm />;
   }
 
---
+++ components/Beta/BetaClosed.js
@@ -0,0 +1,18 @@
+"use client";
+
+import Image from "next/image";
+
+export function BetaClosed() {
+  return (
+    <div>
+      <div className="h-screen w-screen flex justify-center items-center bg-slate-100">
+        <div className="flex flex-col sm:shadow-xl px-8 py-8 bg-white rounded-xl space-y-8 justify-items-center items-center w-4/5 sm:w-3/4 lg:w-2/4 xl:w-4/12 2xl:w-1/4">
+          <Image src="/icon.png" width={64} height={64}></Image>
+          <div className="flex flex-col items-center gap-4 w-full md:w-3/4">
+            <h1>Beta Closed</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
